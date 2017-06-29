dabe025a38a819160b1012c2aa59583372ea1473
ZhaolYang <kero_zhao@163.com>
2017-06-29 23:02:26 +0800

    页面引用svg

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@ import './assets/reset.css'
 import Topbar from './components/Topbar'
 import ResumeEditor from './components/ResumeEditor'
 import ResumePreview from './components/ResumePreview'
+import icons from './assets/icons'
 
 export default {
   name: 'app',
@@ -25,7 +26,10 @@ export default {
       text: '你好'
     }
   },
-  components: {Topbar, ResumeEditor, ResumePreview}
+  components: {Topbar, ResumeEditor, ResumePreview},
+  created(){
+    document.body.insertAdjacentHTML('afterbegin', icons)
+  }
 }
 </script>
 
@@ -60,5 +64,12 @@ export default {
     flex-grow: 1;
     margin-left: 16px;
     background: #777;
+  }
+  svg.icon{
+    height: 1em;
+    width: 1em;
+    fill: currentColor;
+    vertical-align: -0.1em;
+    font-size: 16px;
   }
 </style>
--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -2,17 +2,16 @@
 	<div id="resumeEditor">
 		<nav>
 			<ol>
-				<li v-for="(item,index) in resume.visibleItems"
-				 :class="{active: item === selected}"
-				  @click="selected = item"
-				>
-					{{index}}
+				<li v-for="(item,index) in resume.config" :class="{active: item.field === selected}" @click="selected = item.field">
+					<svg class="icon">
+						<use :xlink:href="`#icon-${item.icon}`"></use>
+					</svg>
 				</li>
 			</ol>
 		</nav>
 		<ol class="pannels">
-			<li v-for="item in resume.visibleItems" v-show="item === selected">
-				{{resume[item]}}
+			<li v-for="item in resume.config" v-show="item.field === selected">
+				{{resume[item.field]}}
 			</li>
 		</ol>
 	</div>
@@ -23,10 +22,17 @@
 		name: 'ResumeEditor',
 		data() {
 			return {
-				selected: 'bio',
+				selected: 'profile',
 				resume: {
-					visibleItems: ['bio', 'work history', 'education', 'projects', 'awards', 'contacts', 'others'],
-					bio: {
+					config: [
+						{ field: 'profile', icon: 'id' },
+						{ field: 'work history', icon: 'work' },
+						{ field: 'education', icon: 'book' },
+						{ field: 'projects', icon: 'heart' },
+						{ field: 'awards', icon: 'cup' },
+						{ field: 'contacts', icon: 'phone' }
+					],
+					profile: {
 						name: '',
 						city: '',
 						title: ''
@@ -35,8 +41,7 @@
 					education: [],
 					projects: [],
 					awards: [],
-					contacts: [],
-					others: []
+					contacts: []
 				}
 			}
 		}
@@ -68,6 +73,10 @@
 					}
 				}
 			}
+		}
+		svg.icon{
+			width: 24px;
+			height: 24px;
 		}
 	}
 	ol{
