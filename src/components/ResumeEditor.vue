d9081f83243a074c45daba3cb393f0bf21820001
ZhaolYang <kero_zhao@163.com>
2017-06-29 23:20:23 +0800

    显示第一个panel内容

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -1,85 +1,107 @@
 <template>
-	<div id="resumeEditor">
-		<nav>
-			<ol>
-				<li v-for="(item,index) in resume.config" :class="{active: item.field === selected}" @click="selected = item.field">
-					<svg class="icon">
-						<use :xlink:href="`#icon-${item.icon}`"></use>
-					</svg>
-				</li>
-			</ol>
-		</nav>
-		<ol class="pannels">
-			<li v-for="item in resume.config" v-show="item.field === selected">
-				{{resume[item.field]}}
-			</li>
-		</ol>
-	</div>
+  <div id="resumeEditor">
+    <nav>
+      <ol>
+        <li v-for="(item,index) in resume.config" :class="{active: item.field === selected}" @click="selected = item.field">
+          <svg class="icon">
+            <use :xlink:href="`#icon-${item.icon}`"></use>
+          </svg>
+        </li>
+      </ol>
+    </nav>
+    <ol class="panels">
+      <li v-for="item in resume.config" v-show="item.field === selected">
+        <div class="resumeField" v-for="(value,key) in resume[item.field]">
+          <label> {{key}} </label>
+          <input type="text" v-model="resume[item.field][key]">
+        </div>
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'ResumeEditor',
-		data() {
-			return {
-				selected: 'profile',
-				resume: {
-					config: [
-						{ field: 'profile', icon: 'id' },
-						{ field: 'work history', icon: 'work' },
-						{ field: 'education', icon: 'book' },
-						{ field: 'projects', icon: 'heart' },
-						{ field: 'awards', icon: 'cup' },
-						{ field: 'contacts', icon: 'phone' }
-					],
-					profile: {
-						name: '',
-						city: '',
-						title: ''
-					},
-					'work history': [],
-					education: [],
-					projects: [],
-					awards: [],
-					contacts: []
-				}
-			}
-		}
-	}
+  export default {
+    name: 'ResumeEditor',
+    data() {
+      return {
+        selected: 'profile',
+        resume: {
+          config: [
+            { field: 'profile', icon: 'id' },
+            { field: 'work history', icon: 'work' },
+            { field: 'education', icon: 'book' },
+            { field: 'projects', icon: 'heart' },
+            { field: 'awards', icon: 'cup' },
+            { field: 'contacts', icon: 'phone' },
+          ],
+          profile: {
+            name: '',
+            city: '',
+            title: ''
+          },
+          'work history': [],
+          education: [],
+          projects: [],
+          awards: [],
+          contacts: [],
+        }
+      }
+    }
+  }
 </script>
 
-<style lang="scss">
-	#resumeEditor{
-		background: #fff;
-		box-shadow: 0 1px 3px 0 rgba(0,0,0,0.25);
-		display: flex;
-		flex-direction: row;
-		overflow: auto;
-		> nav{
-			width: 80px;
-			background: black;
-			color: white;
-			> ol {
-				> li {
-					height: 48px;
-					display: flex;
-					justify-content: center;
-					align-items: center;
-					margin-top: 16px;
-					margin-bottom: 16px;
-					&.active{
-						background: white;
-						color: black;
-					}
-				}
-			}
-		}
-		svg.icon{
-			width: 24px;
-			height: 24px;
-		}
-	}
-	ol{
-		list-style: none;
-	}
+<style lang="scss" scoped>
+  #resumeEditor{
+    background:#ffffff;
+    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+    display: flex;
+    flex-direction: row;
+    overflow: auto;
+    > nav{
+      width: 80px;
+      background: black;
+      color: white;
+      > ol {
+        > li{
+          height: 48px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin-top: 16px;
+          margin-bottom: 16px;
+          &.active{
+            background: white;
+            color: black;
+          }
+        }
+      }
+    }
+    > .panels{
+      flex-grow: 1;
+      > li {
+        padding: 24px;
+      }
+    }
+    svg.icon{
+      width: 24px; // 原设计稿 32px 不好看，改成 24px
+      height: 24px;
+    }
+  }
+  ol{
+    list-style: none;
+  }
+  .resumeField{
+    > label{
+      display: block;
+    }
+    input[type=text]{
+      margin: 16px 0;
+      border: 1px solid #ddd;
+      box-shadow:inset 0 1px 3px 0 rgba(0,0,0,0.25);
+      width: 100%;
+      height: 40px;
+      padding: 0 8px;
+    }
+  }
 </style>
