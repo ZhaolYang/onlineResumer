be2d8cc08401ceb512172330a22c1d9e849c932e
ZhaolYang <kero_zhao@163.com>
2017-06-29 22:23:54 +0800

    添加左侧导航

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,36 +1,61 @@
 <template>
-	<div id="topbar">
-		<div class="wrapper">
-			<span class="logo">Resumer</span>
-		</div>
-	</div>
+  <div id="topbar">
+    <div class="wrapper">
+      <span class="logo">Resumer</span>
+
+      <div class="actions">
+        <button class="primary">保存</button>
+        <button>预览</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'Topbar'
-	}
+export default {
+  name: 'Topbar'
+}
 </script>
 
-<style>
-	#topbar {
+<style scoped lang="scss">
+	// 自行查阅 scoped 的功能
+    // 见：https://cn.vuejs.org/v2/guide/comparison.html#CSS-的组件作用域
+	#topbar{
 		background: #fff;
 		box-shadow: 0 1px 3px 0 rgba(0,0,0,0.25);
+		>.wrapper{
+			min-width: 1024px;
+			max-width: 1440px;
+			margin: 0 auto;
+			height: 64px;
+		}
+		>.wrapper{
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			padding: 0 16px;
+		}
+		.logo{
+			font-size: 24px;
+			color: #000;
+		}
 	}
-	#topbar>.wrapper{
-		min-width: 1024px;
-		max-width: 1440px;
-		margin: 0 auto;
-		height: 64px;
-	}
-	#topbar>.wrapper{
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		padding: 0 16px;
-	}
-	#topbar .logo{
-		font-size: 24px;
-		color: #000;
+
+	button{
+		width: 72px;
+		height: 32px;
+		border: none;
+		cursor: pointer;
+		font-size: 18px;
+		background: #ddd;
+		color: #222;
+		&:hover{
+			box-shadow: 1px 1px 1px hsla(0, 0, 0, 0.5);
+		}
+		&.primary{
+			background: #02af5f;
+			color: white;
+		}
+
 	}
 </style>
